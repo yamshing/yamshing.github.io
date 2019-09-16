bc6594e55a529930269498896cf71b7914bb3734
yamshing <yamshing@gmail.com>
2019-09-16 17:33:15 +0900

    fruits tap game page add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 (function() {
-  var get_is_jp, get_language;
+  var get_is_jp, get_language, toggle_list;
 
   get_language = function() {
     var e;
@@ -20,10 +20,35 @@
     }
   };
 
+  toggle_list = function(list, is_jp) {
+    var link;
+    link = $(list).attr('link');
+    if (link.match(/_jp.html/)) {
+      if (is_jp) {
+        return $(list).show();
+      } else {
+        return $(list).hide();
+      }
+    } else {
+      if (is_jp) {
+        return $(list).hide();
+      } else {
+        return $(list).show();
+      }
+    }
+  };
+
   $(document).ready(function() {
-    var is_jp;
+    var app_list_arr, i, is_jp, len, list, results;
     is_jp = get_is_jp();
-    return console.log("is_jp (in index.coffee) ", is_jp);
+    console.log("is_jp (in index.coffee) ", is_jp);
+    app_list_arr = $('.app_list');
+    results = [];
+    for (i = 0, len = app_list_arr.length; i < len; i++) {
+      list = app_list_arr[i];
+      results.push(toggle_list(list, is_jp));
+    }
+    return results;
   });
 
 }).call(this);
